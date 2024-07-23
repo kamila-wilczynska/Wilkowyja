99b9b35b5d653e491766b6915c10936ad758975a
Kamila W <kamila.wilczynska92@gmail.com>
2024-07-23 16:15:59 +0100

    fix homepage scroll

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,13 @@ import Services from "../components/Services";
 import FeaturedRoom from "../components/FeaturedRoom";
 import Hero from "../components/Hero";
 import MapComponent from "../components/MapComponent/MapComponent";
+import { useEffect } from "react";
 
 function Home() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div>
       <Hero />
