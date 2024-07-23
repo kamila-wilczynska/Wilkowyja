ee67fdd707e486fb8cedc6b8c9ef052d420dc143
Kamila W <kamila.wilczynska92@gmail.com>
2024-07-23 16:42:52 +0100

    add call function

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,56 @@
+// import logo from "/logo.png";
+// import { FaAlignRight } from "react-icons/fa";
+// import { Link } from "react-router-dom";
+// import { useState } from "react";
+
+// function NavBar() {
+//   const [show, setShow] = useState(false);
+
+//   function handleToggle() {
+//     setShow(!show);
+//   }
+
+//   function handleLinkClick() {
+//     setShow(false);
+//   }
+
+//   return (
+//     <nav className="navbar">
+//       <div className="nav-center">
+//         <div className="nav-header">
+//           <Link to="/">
+//             <img className="logo" src={logo} alt="wylkowyja logo" />
+//           </Link>
+//           <button type="button" className="nav-btn" onClick={handleToggle}>
+//             <FaAlignRight className="nav-icon" />
+//             <span>Menu</span>
+//           </button>
+//         </div>
+//         <ul className={show ? "nav-links show-nav" : "nav-links"}>
+//           <li>
+//             <Link to="/" onClick={handleLinkClick}>
+//               Home
+//             </Link>
+//           </li>
+//           <li>
+//             <Link to="/gallery" onClick={handleLinkClick}>
+//               Galeria
+//             </Link>
+//           </li>
+//           <li>
+//             <Link to="/attractions" onClick={handleLinkClick}>
+//               Atrakcje
+//             </Link>
+//           </li>
+//         </ul>
+//       </div>
+//     </nav>
+//   );
+// }
+
+// export default NavBar;
 import logo from "/logo.png";
-import { FaAlignRight } from "react-icons/fa";
+import { FaAlignRight, FaPhone } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 
@@ -41,6 +92,11 @@ function NavBar() {
             <Link to="/attractions" onClick={handleLinkClick}>
               Atrakcje
             </Link>
+          </li>
+          <li className="mobile-only">
+            <a href="tel:+123456789" className="reserve-btn">
+              <FaPhone /> Zadzwoń
+            </a>
           </li>
         </ul>
       </div>
